21990cde37d38280788a107380adf3bd762b30dc
Bex Edmondson <bex.edmondson@hutchgames.com>
2019-03-30 12:26:02 +0000

    adding Screen, UIManager, and ScreenType scripts

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour 
+{
+	[System.Serializable]
+	class ScreenTypePair
+	{
+		[SerializeField]
+		private ScreenType type;
+
+		[SerializeField]
+		private Screen screenObject;
+	}
+
+	[SerializeField]
+	private List<ScreenTypePair> screenTypePairs = new List<ScreenTypePair> { };
+}

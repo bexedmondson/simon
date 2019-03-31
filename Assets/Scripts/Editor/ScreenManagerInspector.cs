6a15049bb3cf310866fc2465ab3f215511c826cd
Bex Edmondson <bex.edmondson@hutchgames.com>
2019-03-31 22:34:17 +0100

    improving screen manager editor
    Can no longer use screen types more than once in the list - this way, when switching to a screen type, there won't be confusion about which screen to activate.

--- Assets/Scripts/Editor/ScreenManagerInspector.cs
+++ Assets/Scripts/Editor/ScreenManagerInspector.cs
@@ -11,8 +11,12 @@ public class ScreenManagerInspector : Editor
     {
 		static List<bool> expansionBoolList = new List<bool> { };
 
+		private static SerializedProperty typePairList = null;
+
 		public static void ShowList( SerializedProperty list )
         {
+			typePairList = list;
+			
 			EditorGUILayout.PropertyField( list );
 
 			if( list.isExpanded )
@@ -36,55 +40,85 @@ public class ScreenManagerInspector : Editor
                 
 				for( int i = 0; i < list.arraySize; i++ )
 				{
-					expansionBoolList[i] = ScreenTypePairInspector.Show( list.GetArrayElementAtIndex( i ), expansionBoolList[i] );
+					expansionBoolList[i] = ShowTypeObjectPair( list.GetArrayElementAtIndex( i ), expansionBoolList[i] );
 				}
 
 				EditorGUI.indentLevel -= 1;
 			}
         }
-    }
-    
-	public static class ScreenTypePairInspector
-	{      
-		public static bool Show(SerializedProperty pair, bool expanded)
-		{
-			SerializedProperty screenType = pair.FindPropertyRelative( "type" );
+
+		public static bool ShowTypeObjectPair( SerializedProperty pair, bool expanded )
+        {
+            SerializedProperty screenType = pair.FindPropertyRelative( "type" );
             SerializedProperty screenObject = pair.FindPropertyRelative( "screenObject" );
 
-			string foldoutName = "";
-            
-			if( screenType == null || screenType.objectReferenceValue == null )
-				foldoutName = "No ScreenType Assigned";
-			else
-				foldoutName += screenType.objectReferenceValue.ToString();
+            string foldoutName = "";
 
-			if( screenObject == null || screenObject.objectReferenceValue == null )
-				foldoutName += " - No ScreenObject Assigned!";
+            if( screenType == null || screenType.objectReferenceValue == null )
+                foldoutName = "No ScreenType Assigned";
+            else
+                foldoutName += screenType.objectReferenceValue.ToString();
 
-			bool expandedToReturn = EditorGUILayout.Foldout( expanded, foldoutName );
+            if( screenObject == null || screenObject.objectReferenceValue == null )
+                foldoutName += " - No ScreenObject Assigned!";
 
-			if( expandedToReturn )
+            bool expandedToReturn = EditorGUILayout.Foldout( expanded, foldoutName );
+
+            if( expandedToReturn )
+            {
+                EditorGUILayout.BeginHorizontal();
+
+                GUILayout.Space( 20 );
+
+                EditorGUILayout.BeginVertical();
+                GUILayout.Label( "Screen Type", GUILayout.Width( 75 ) );
+
+                EditorGUI.BeginChangeCheck();
+				Object previousScreenTypeObject = screenType.objectReferenceValue;
+
+                EditorGUILayout.PropertyField( screenType, GUIContent.none );
+                if( EditorGUI.EndChangeCheck() )
+                {
+					if( IsScreenTypeAlreadyInList( screenType ) )
+					{
+						screenType.objectReferenceValue = previousScreenTypeObject;
+						EditorUtility.DisplayDialog( "Can't add this ScreenType!", "This screen type is already beign used elsewhere. Please make another screen type (Create>Screen Type) or change this to an unused screen type.", "OK" );
+						//Debug.LogWarning( "Cannot add this screen type - it's already being used for another list!" );
+					}
+                }
+
+                EditorGUILayout.EndVertical();
+
+                EditorGUILayout.BeginVertical();
+                GUILayout.Label( "ScreenObject", GUILayout.Width( 75 ) );
+                EditorGUILayout.PropertyField( screenObject, GUIContent.none );
+                EditorGUILayout.EndVertical();
+
+                EditorGUILayout.EndHorizontal();
+            }
+
+            return expandedToReturn;
+        }
+
+		private static bool IsScreenTypeAlreadyInList(SerializedProperty screenTypeToCheckFor)
+		{
+			int numOfTimesTypeIsInList = 0;
+			
+			for( int i = 0; i < typePairList.arraySize; i++ )
 			{
-				EditorGUILayout.BeginHorizontal();
-
-				GUILayout.Space( 20 );
-
-				EditorGUILayout.BeginVertical();
-				GUILayout.Label( "Screen Type", GUILayout.Width( 75 ) );
-				EditorGUILayout.PropertyField( screenType, GUIContent.none );
-				EditorGUILayout.EndVertical();
-
-				EditorGUILayout.BeginVertical();
-				GUILayout.Label( "ScreenObject", GUILayout.Width( 75 ) );
-				EditorGUILayout.PropertyField( screenObject, GUIContent.none );
-				EditorGUILayout.EndVertical();
-
-				EditorGUILayout.EndHorizontal();
+				if( typePairList.GetArrayElementAtIndex( i ).FindPropertyRelative( "type" ) != null && typePairList.GetArrayElementAtIndex( i ).FindPropertyRelative( "type" ).objectReferenceValue != null )
+				{
+					if( typePairList.GetArrayElementAtIndex( i ).FindPropertyRelative( "type" ).objectReferenceValue == screenTypeToCheckFor.objectReferenceValue )
+					{
+						numOfTimesTypeIsInList++;
+					}
+				}
 			}
 
-			return expandedToReturn;
+            //if it's in the list more than once, the screen type is being used somewhere other than the place it's just been entered
+			return ( numOfTimesTypeIsInList > 1 );
 		}
-	}
+    }
 
 #endregion
 

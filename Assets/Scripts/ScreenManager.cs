3bb3b371cd016f4adff7bed96c4d8e91ab01b2e0
Bex Edmondson <bex.edmondson@hutchgames.com>
2019-03-31 22:53:17 +0100

    Making ScreenManager a singleton
    Don't need more than one screenmanager editing the screens!

--- Assets/Scripts/ScreenManager.cs
+++ Assets/Scripts/ScreenManager.cs
@@ -16,4 +16,17 @@ public class ScreenManager : MonoBehaviour
 
 	[SerializeField]
 	private List<ScreenTypePair> screenTypePairs = new List<ScreenTypePair> { };
+
+	private static ScreenManager instance = null;
+    
+	void Awake()
+    {
+		//Making this a singleton so only one thing is changing the screens at once.
+		if( instance == null )         
+            instance = this;
+        else if( instance != this )
+            Destroy( gameObject );
+
+        DontDestroyOnLoad( gameObject );
+    }
 }

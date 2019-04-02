55c994f56636b9ffbd5ebdd8b31671c2fde3b33b
Bex Edmondson <bex.edmondson@hutchgames.com>
2019-04-02 20:22:53 +0100

    adding basic start screen list to ensure the sequence in progress is cleared

---
+++ Assets/Scripts/ScreenSystem/Screens/StartScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Note: In a normal project, I would separate UI and game logic, but this game is 
+// entirely built in UI so I'm putting more logic in this script than I usually would.
+public class StartScreen : Screen 
+{
+	[SerializeField]
+	private NoteList sequenceInProgress;
+
+	private void OnEnable()
+	{
+		//Whenever this screen is shown, the sequence should be cleared
+		sequenceInProgress.noteList.Clear();
+	}
+}

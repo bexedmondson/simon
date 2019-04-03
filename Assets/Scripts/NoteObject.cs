4469c0d000eab40b749dba623985d162d8e59ca5
Bex Edmondson <bex.edmondson@hutchgames.com>
2019-04-03 01:20:38 +0100

    adding in piano images to tune-playing screen and note object script

---
+++ Assets/Scripts/NoteObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class NoteObject : MonoBehaviour 
+{
+	[SerializeField]
+	private Note note;
+
+	   
+}

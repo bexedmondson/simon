5fb9af5e37304b4f01af85f49000a557e7c16ae1
Bex Edmondson <bex.edmondson@hutchgames.com>
2019-04-06 19:20:59 +0100

    Editing note object script to play a clip on "play"

--- Assets/Scripts/NoteObject.cs
+++ Assets/Scripts/NoteObject.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
 public class NoteObject : MonoBehaviour 
 {
 	[SerializeField]
@@ -10,15 +11,19 @@ public class NoteObject : MonoBehaviour
 
 	private Animator animator;
 
+	private AudioSource audioSource;
+
 	public Note Note { get { return note; } }
 
 	private void Awake()
 	{
 		animator = GetComponent<Animator>();
+		audioSource = GetComponent<AudioSource>();
 	}
 
 	public void Play()
 	{
 		animator.SetTrigger( "Play" );
+		audioSource.PlayOneShot( audioSource.clip );
 	}
 }

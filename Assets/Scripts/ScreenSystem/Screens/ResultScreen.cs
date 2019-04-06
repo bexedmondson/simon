a5b372c3283404bf0586aec060dcfe102344a95b
Bex Edmondson <bex.edmondson@hutchgames.com>
2019-04-06 18:46:13 +0100

    Adding very basic result display

---
+++ Assets/Scripts/ScreenSystem/Screens/ResultScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultScreen : Screen 
+{
+	[SerializeField]
+	private NoteList sequenceJustPlayed;
+
+	[SerializeField]
+	private Text resultNumberText;
+    
+	private void OnEnable()
+	{
+		//When this screen is transitioned to, display the length of the sequence they successfully completed.
+		resultNumberText.text = (sequenceJustPlayed.noteList.Count - 1).ToString();
+	}
+}

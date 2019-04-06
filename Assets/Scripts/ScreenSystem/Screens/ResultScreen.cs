92717462f02b2d8fd6f7966624738000d1e9da4a
Bex Edmondson <bex.edmondson@hutchgames.com>
2019-04-06 19:02:38 +0100

    Adding quit button to results screen

--- Assets/Scripts/ScreenSystem/Screens/ResultScreen.cs
+++ Assets/Scripts/ScreenSystem/Screens/ResultScreen.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ResultScreen : Screen 
+public class ResultScreen : Screen
 {
 	[SerializeField]
 	private NoteList sequenceJustPlayed;
 
 	[SerializeField]
 	private Text resultNumberText;
-    
+
 	private void OnEnable()
 	{
 		//When this screen is transitioned to, display the length of the sequence they successfully completed.
-		resultNumberText.text = (sequenceJustPlayed.noteList.Count - 1).ToString();
+		resultNumberText.text = ( sequenceJustPlayed.noteList.Count - 1 ).ToString();
+	}
+
+	public void ExitGame()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
 	}
 }

315d5981563bf6b3d4c7032848ceb21daafdbe40
Bex Edmondson <bex.edmondson@hutchgames.com>
2019-04-07 16:06:21 +0100

    Adding difficulty setting scripts and scriptableobjects and hooking up to settings screen

---
+++ Assets/Scripts/Difficulty/DifficultyButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO: Make this inherit from Button instead, and change OnSelectDifficulty to override OnClick
+public class DifficultyButton : MonoBehaviour 
+{
+	[SerializeField]
+	private DifficultySettingHolder settingHolder;
+
+	[SerializeField]
+	private DifficultySetting difficultySetting;
+
+	public void OnSelectDifficulty()
+	{
+		settingHolder.setting = difficultySetting;
+	}
+}
---
+++ Assets/Scripts/Difficulty/DifficultySetting.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DifficultySetting : ScriptableObject 
+{
+	[SerializeField]
+	private float timeBetweenNotes;
+
+	public float TimeBetweenNotes { get { return timeBetweenNotes; } }
+}
